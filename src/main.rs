ff0c14913652d3ec94db5fc4d5ea04ed2c1427f0
Shuaipeng Yu <jackysp@gmail.com>
2021-03-04 19:03:42 +0800

    support interval
    Signed-off-by: Shuaipeng Yu <jackysp@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate clap;
+extern crate chrono;
 
 use std::process;
-use clap::{crate_authors, App, Arg};
+use std::thread;
+use clap::{crate_authors, App, Arg, ArgMatches};
+use chrono::SecondsFormat;
 
 fn main() {
     let version = "1.0.0";
@@ -23,10 +26,43 @@ fn main() {
                 .help("To be replaced by your unique API key. Visit the API Manager page within your account for details.")
                 .default_value("12345")
         )
+        .arg(
+            Arg::with_name("domain")
+                .short("d")
+                .long("domain")
+                .help("The domain associated with the DNS resource record to be modified.")
+                .default_value("namesilo.com")
+        )
+        .arg(
+            Arg::with_name("hostname")
+                .short("h")
+                .long("hostname")
+                .help("The hostname to use (there is no need to include the ).")
+                .default_value("www")
+        )
+        .arg(
+            Arg::with_name("interval")
+                .short("i")
+                .long("interval")
+                .help("The seconds of updating interval.")
+                .default_value("10")
+        )
         .get_matches();
 
     if matches.is_present("version") {
         println!("{}", version);
         process::exit(0);
     }
+    update_dns_loop(matches)
+}
+
+fn update_dns_loop(args: ArgMatches) {
+    loop {
+        update_dns();
+        thread::sleep(chrono::Duration::seconds(args.value_of("interval").unwrap().parse::<i64>().unwrap()).to_std().unwrap());
+    }
+}
+
+fn update_dns() {
+    println!("{}", chrono::Local::now().to_rfc3339_opts(SecondsFormat::Millis, false))
 }
